839244e6b5b900b3dd601232a2435ed87f4137d1
darrific <darrienpersad@gmail.com>
2021-04-08 00:03:58 -0400

    Firebase Connected, New Landing Done

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let database = firebase.database();
-M.AutoInit();
 
-function registerUser(name, phone, vehicle, dp, area) {
-   firebase.database().ref('drivers/' + vehicle).set({
+async function registerUser(name, phone, vehicle, dp, area) {
+   await firebase.database().ref('drivers/' + vehicle).set({
       name: name,
       phone: phone,
       dp: dp, 
@@ -23,36 +22,67 @@ var getDrivers = firebase.database().ref('drivers/');
 getDrivers.on('value', (snapshot) => {
    const data = snapshot.val();
    for(let i in data){
-      regions[data[i].area].push(data[i])
+      let x = data[i]
+      x.vehicle = i.slice(0,3) + " " + i.slice(3)
+      regions[data[i].area].push(x)
    }
 });
 
 console.log(regions)
-
-
-// Tabbed Menu
-function openMenu(evt, menuName) {
-   var i, x, tablinks;
-   x = document.getElementsByClassName("menu");
-   for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
+function areaSelect(){
+   let area = document.getElementById('landingSelector').value
+   let areaName = (area == 'North' || area == 'West' || area == 'East')? "the "+ area : area
+   let temp = `
+      <h4>Drivers in ${areaName}</h4>
+         <div id="driverList"></div>
+      </div>
+   `
+   document.getElementById('results').innerHTML = temp
+   temp = ''
+   for(let i of regions[area]){
+      console.log(i.area)
+      temp+=`
+         <div class="driverListItem">
+            <img class='listItemVerifiedBadge' src="./pictures/protection.png" alt="">
+            <div class="listItemInner">
+               <b>${i.name}</b>
+               <b>Vehicle License: ${i.vehicle}</b>
+            </div>
+         </div>
+      `
    }
-   tablinks = document.getElementsByClassName("tablink");
-   for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
-   }
-   document.getElementById(menuName).style.display = "block";
-   evt.currentTarget.firstElementChild.className += " w3-red";
+   document.getElementById('driverList').innerHTML = temp
 }
-document.getElementById("myLink").click();
 
 document.getElementById('registerBtn').addEventListener('click', (event)=>{
    event.preventDefault()
-   let name = document.getElementById('name').value
-   let phone = document.getElementById('phone').value
-   let vehicle = document.getElementById('vehicle').value
-   let dp = document.getElementById('dp').value
-   let area = document.getElementById('area').value
-   console.log(name,phone,vehicle,dp,area)
-   registerUser(name, phone, vehicle, dp, area)
+   let name = document.getElementById('name')
+   let phone = document.getElementById('phone')
+   let vehicle = document.getElementById('vehicle')
+   let dp = document.getElementById('dp')
+   let area = document.getElementById('area')
+   registerUser(name.value, phone.value, vehicle.value, dp.value, area.value)
+   name.value = ''
+   phone.value = ''
+   vehicle.value = ''
+   dp.value = ''
+   area.value = ''
 })
+
+
+
+// // Tabbed Menu
+// function openMenu(evt, menuName) {
+//    var i, x, tablinks;
+//    x = document.getElementsByClassName("menu");
+//    for (i = 0; i < x.length; i++) {
+//       x[i].style.display = "none";
+//    }
+//    tablinks = document.getElementsByClassName("tablink");
+//    for (i = 0; i < x.length; i++) {
+//       tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+//    }
+//    document.getElementById(menuName).style.display = "block";
+//    evt.currentTarget.firstElementChild.className += " w3-red";
+// }
+// document.getElementById("myLink").click();
